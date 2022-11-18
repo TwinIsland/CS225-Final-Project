199849c60866195a5093af61ebaa84fa5349da6d
Jinheng Li <jinheng2@illinois.edu>
2022-11-18 20:51:07 +0000

    updated makefile

--- entry/main.cc
+++ entry/main.cc
@@ -1,4 +1,3 @@
-#include "catch.hpp"
 #include "solution.hpp"
 
 #include "include.hpp"
@@ -14,44 +13,45 @@ using std::cout;
 using std::endl;
 
 int main(){
-    vector<inputVertex> i_node;
-    vector<Edge> i_edge;
-    string n_string = file_to_string("../data/vertex.csv");
-    string e_string = file_to_string("../data/edge.csv");
+    // vector<inputVertex> i_node;
+    // vector<Edge> i_edge;
+    // string n_string = file_to_string("../data/vertex.csv");
+    // string e_string = file_to_string("../data/edge.csv");
 
-    vector<string> line_node;
-    SplitString(n_string, '\n', line_node);
-    vector<string> line_edge;
-    SplitString(n_string, '\n', line_edge);
+    // vector<string> line_node;
+    // SplitString(n_string, '\n', line_node);
+    // vector<string> line_edge;
+    // SplitString(n_string, '\n', line_edge);
 
-    for (string s : line_node) {
-        inputVertex iv;
-        SplitString(s, '\n', iv);
-        i_node.push_back(iv);
-    }
-    for (string s : line_edge) {
-        inputVertex ivfrom;
-        inputVertex ivto;
-        vector<string> temp;
-        SplitString(s, '\n', temp);
-        ivfrom.push_back(temp[0]);
-        ivto.push_back(temp[1]);
-        double wei = std::stod(temp[2]);
-        Edge edge__;
-        edge__.from = ivfrom;
-        edge__.to = ivto;
-        edge__.weight = wei;
-        i_edge.push_back(edge__);
-    }
+    // for (string s : line_node) {
+    //     inputVertex iv;
+    //     SplitString(s, '\n', iv);
+    //     i_node.push_back(iv);
+    // }
+    // for (string s : line_edge) {
+    //     inputVertex ivfrom;
+    //     inputVertex ivto;
+    //     vector<string> temp;
+    //     SplitString(s, '\n', temp);
+    //     ivfrom.push_back(temp[0]);
+    //     ivto.push_back(temp[1]);
+    //     double wei = std::stod(temp[2]);
+    //     Edge edge__;
+    //     edge__.from = ivfrom;
+    //     edge__.to = ivto;
+    //     edge__.weight = wei;
+    //     i_edge.push_back(edge__);
+    // }
 
-    Graph_directed g(i_node,i_edge);
-    for (string s : g.nodes) {
-        Graph_directed::Vertex v = g.getVertex(s);
-        cout << v.name_ << endl;
-        for (pair<string,double> p : v.neighors_) {
-            cout << p.first << " " << p.second << " ";
-        }
-        cout << endl;
-    }
+    // Graph_directed g(i_node,i_edge);
+    // for (string s : g.nodes) {
+    //     Graph_directed::Vertex v = g.getVertex(s);
+    //     cout << v.name_ << endl;
+    //     for (pair<string,double> p : v.neighors_) {
+    //         cout << p.first << " " << p.second << " ";
+    //     }
+    //     cout << endl;
+    // }
+    std::cout << "Shit" << std::endl;
     return 0;
 }
--- src/driver.cc
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <vector>
-
-#include "solution.hpp"
-
-
-int main() {
-  std::cout << "hello world" << std::endl;
-}
