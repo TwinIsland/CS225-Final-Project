6beaf2b6193ddf76689b48fc71bc302f15d9eda7
zzh <zihao10@illinois.edu>
2022-11-18 02:48:27 +0000

    toz-mac-11-17-0848pm

--- tests/tests.cc
+++ tests/tests.cc
@@ -1,7 +1,23 @@
 #include "catch.hpp"
 #include "solution.hpp"
 
+#include "include.hpp"
+#include "graph.h"
+
+using std::string;
+using std::set;
+using std::vector;
+using std::pair;
+using std::list;
+
 
 TEST_CASE("test1") {
-    REQUIRE(true == false);
+    REQUIRE(true == true);
+}
+
+TEST_CASE("readfile and map construct") {
+    vector<inputVertex> i_node;
+    vector<Edge> i_edge;
+    
+    
 }
