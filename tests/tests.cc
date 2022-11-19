6caab053e040c81a812797070ccc75d5ac5b65bd
Jinheng Li <jinheng2@illinois.edu>
2022-11-19 00:28:58 +0000

    push obj folder and update test cases

--- tests/tests.cc
+++ tests/tests.cc
@@ -14,48 +14,32 @@ using std::cout;
 using std::endl;
 
 
+// global object graph (save runtime)
+Graph_directed graph ("./data/vertex.csv" , "./data/edge.csv");
 
-TEST_CASE("test1") {
-    REQUIRE(true == true);
+void print_neighbors(std::vector<std::pair<std::string, double>> neighbors) {
+    for (auto it = neighbors.begin(); it != neighbors.end(); it++) {
+        std::cout << it->first << " ";
+    }
 }
 
-TEST_CASE("readfile and map construct") {
-    vector<inputVertex> i_node;
-    vector<Edge> i_edge;
-    string n_string = file_to_string("../data/vertex.csv");
-    string e_string = file_to_string("../data/edge.csv");
+TEST_CASE("# neighbors on exists") {
+    std::vector<std::pair<std::string, double>> neighbors = graph.get_neighbors("POR");
+    REQUIRE(neighbors.size() == 3);
 
-    vector<string> line_node;
-    SplitString(n_string, '\n', line_node);
-    vector<string> line_edge;
-    SplitString(n_string, '\n', line_edge);
+    neighbors = graph.get_neighbors("YQQ");
+    REQUIRE(neighbors.size() == 4);
+    print_neighbors(neighbors);
 
-    for (string s : line_node) {
-        inputVertex iv;
-        SplitString(s, '\n', iv);
-        i_node.push_back(iv);
-    }
-    for (string s : line_edge) {
-        inputVertex ivfrom;
-        inputVertex ivto;
-        vector<string> temp;
-        SplitString(s, '\n', temp);
-        ivfrom.push_back(temp[0]);
-        ivto.push_back(temp[1]);
-        double wei = std::stod(temp[2]);
-        Edge edge__;
-        edge__.from = ivfrom;
-        edge__.to = ivto;
-        edge__.weight = wei;
-        i_edge.push_back(edge__);
-    }
+    neighbors = graph.get_neighbors("NBC");
+    REQUIRE(neighbors.size() == 7);
+}
 
-    Graph_directed g(i_node,i_edge);
-    for (Graph_directed::Vertex v : g.all_node) {
-        cout << v.name_ << endl;
-        for (pair<string,double> p : v.neighors_) {
-            cout << p.first << " " << p.second << " ";
-        }
-        cout << endl;
-    }
+TEST_CASE("# neighbors on non_exist") {
+    std::vector<std::pair<std::string, double>> neighbors = graph.get_neighbors("HZK");
+    REQUIRE(neighbors.size() == 0);
+
+    neighbors = graph.get_neighbors("AAA");
+    REQUIRE(neighbors.size() == 0);
+
 }
