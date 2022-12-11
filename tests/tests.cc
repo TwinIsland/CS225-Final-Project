ef7a47f52be2208d4f284d7f91c87a79d957e40a
root <root@Erdao.localdomain>
2022-12-11 01:22:29 +0000

    add unit test

--- tests/tests.cc
+++ tests/tests.cc
@@ -57,11 +57,11 @@ TEST_CASE("bfs") {
 
     REQUIRE(b.size() == 3);
 
-    std::vector<string> c = graph.bfs_one_component("A");
+    std::vector<string> c = graph.bfs_one_component("C");
 
     REQUIRE(c.size() == 2);
 
-    std::vector<string> d = graph.bfs_one_component("A");
+    std::vector<string> d = graph.bfs_one_component("D");
 
     REQUIRE(d.size() == 2);
 }
@@ -71,36 +71,25 @@ TEST_CASE("bfs") {
 TEST_CASE("shortest path table test") {
     std::vector<string> path1;
     // make the path
-    path1.push_back("a");
-    path1.push_back("b");
-    path1.push_back("c");
+    path1.push_back("B");
+    path1.push_back("C");
     
-    auto out = graph.gen_shortest_path_table();
+    auto spt = graph.gen_shortest_path_table();
 
-    REQUIRE(out["a"]["b"].first == path1);
-    REQUIRE(out["a"]["b"].second == 0.3);
-}
+    auto out = graph.get_shortest_path(spt, "A", "C");
+    double out2 = graph.get_shortest_weight(spt, "A", "C");
 
-TEST_CASE("shortest path table test2") {
-    std::vector<string> path1;
-    // make the path
-    path1.push_back("d");
-    path1.push_back("c");
-    
-    auto out = graph.gen_shortest_path_table();
-
-    REQUIRE(out["d"]["c"].first == path1);
-    REQUIRE(out["a"]["b"].second == 0.5);
+    REQUIRE(out == path1);
+    REQUIRE(out2 - 0.3 < 0.01);
 }
 
 TEST_CASE("shortest path table test3") {
     std::vector<string> path1;
     // make the path
-    path1.push_back("a");
-    path1.push_back("b");
+    path1.push_back("B");
     
     auto out = graph.gen_shortest_path_table();
 
-    REQUIRE(out["a"]["b"].first == path1);
-    REQUIRE(out["a"]["b"].second == 0.1);
+    REQUIRE(out["A"]["B"].first == path1);
+    REQUIRE(out["A"]["B"].second == 0.1);
 }
